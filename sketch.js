84bfbcd51a0ec5d465b4ab171eddfbbf9fb25966
snacce <prod64n746r3m71n@gmail.com>
2024-05-23 12:39:13 +1200

    create diamond prism, center canvas

--- sketch.js
+++ sketch.js
@@ -1,37 +1,91 @@
+var side
+var height
+
+var x1
+var x2
+var x3
+
+var y1
+var y2
+var y3
+
+var z1
+var z2
+var z3
+
+
 function setup() {
-    var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
-  cnv.style('display', 'block');
-  }
+createCanvas(500, 500, WEBGL);
+
+side = 150;
   
-  function draw() {
-    background(255);
-    
-    // Rotate the canvas
-    rotateX(frameCount*0.0025);
-    rotateY(frameCount*0.001    );
-    
-    // Coordinates for verteces of the pyramid
+// Calculate the height of the equilateral triangle
+height = sqrt(3) / 2 * side;
 
-    // plain
+noFill();
 
-    line(0,0,0,150,0,0);
-    line(150,0,0,150,150,0);
-    line(150,150,0,0,150,0);
-    line(0,150,0,0,0,0);
+// Set the coordinates of the triangle vertices
+x1 = 100, y1 = 50, z1 = 0;
+x2 = x1 + side, y2 = y1; z2 = z1 + side
+x3 = x1 + side / 2, y3 = y1 + height, z3 = z1 + side / 2;
 
-    // top triangle verticies
 
-    line(0,0,0,75,75,150);
-    line(150,0,0,75,75,150);
-    line(150,150,0,75,75,150);
-    line(0,150,0,75,75,150);
+}
 
-    // bottom verticies
+function draw() {
+  background(255);
 
-    line(0,0,0,75,75,-150);
-    line(150,0,0,75,75,-150);
-    line(150,150,0,75,75,-150);
-    line(0,150,0,75,75,-150);
-    
+  let options = {
+    disableTouchActions: false,
+    freeRotation: true
+  };
+
+   // Define the vertices of the diamond-shaped prism
+   let side = 150;
+   let height = sqrt(3) / 2 * side;
+   let halfHeight = height / 2;
+ 
+   let vertices = [
+     createVector(-side / 2, -halfHeight, 0),
+     createVector(side / 2, -halfHeight, 0),
+     createVector(0, halfHeight, 0),
+     createVector(0, 0, -height / sqrt(2)),
+     createVector(0, 0, height / sqrt(2))
+   ];
+ 
+   // Draw the diamond-shaped prism
+   beginShape(TRIANGLES);
    
-  }
+   // Bottom half of the diamond
+   vertex(vertices[0].x, vertices[0].y, vertices[0].z);
+   vertex(vertices[1].x, vertices[1].y, vertices[1].z);
+   vertex(vertices[3].x, vertices[3].y, vertices[3].z);
+ 
+   vertex(vertices[1].x, vertices[1].y, vertices[1].z);
+   vertex(vertices[2].x, vertices[2].y, vertices[2].z);
+   vertex(vertices[3].x, vertices[3].y, vertices[3].z);
+ 
+   vertex(vertices[2].x, vertices[2].y, vertices[2].z);
+   vertex(vertices[0].x, vertices[0].y, vertices[0].z);
+   vertex(vertices[3].x, vertices[3].y, vertices[3].z);
+ 
+   // Top half of the diamond
+   vertex(vertices[0].x, vertices[0].y, vertices[0].z);
+   vertex(vertices[1].x, vertices[1].y, vertices[1].z);
+   vertex(vertices[4].x, vertices[4].y, vertices[4].z);
+ 
+   vertex(vertices[1].x, vertices[1].y, vertices[1].z);
+   vertex(vertices[2].x, vertices[2].y, vertices[2].z);
+   vertex(vertices[4].x, vertices[4].y, vertices[4].z);
+ 
+   vertex(vertices[2].x, vertices[2].y, vertices[2].z);
+   vertex(vertices[0].x, vertices[0].y, vertices[0].z);
+   vertex(vertices[4].x, vertices[4].y, vertices[4].z);
+   
+   endShape(CLOSE);
+
+   orbitControl(1, 1, 1, options);
+ 
+  
+ 
+}
